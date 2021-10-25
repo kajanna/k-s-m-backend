ee79287c719dbdd76e8d60fdf8fc9b5aafef5bf7
Kaja Anna <kaja.szokalska@gmail.com>
2021-10-25 16:18:55 +0200

    add send-mail part

---
+++ controllers/send-mail.js
@@ -0,0 +1,29 @@
+const HttpError = require('../models/http-error');
+const nodemailer = require('nodemailer');
+
+const sendMessage = (req, res, next) => {
+   
+let mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'xyz@gmail.com',
+        pass: '*************'
+    }
+});
+
+let mailDetails = {
+    from: 'xyz@gmail.com',
+    to: 'abc@gmail.com',
+    subject: 'Test mail',
+    text: 'Node.js testing mail for GeeksforGeeks'
+};
+mailTransporter.sendMail(mailDetails, function(err, data) {
+    if(err) {
+        console.log('Error Occurs');
+    } else {
+        console.log('Email sent successfully');
+    }
+});     
+}
+
+module.exports = sendMessage;
