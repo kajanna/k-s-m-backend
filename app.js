676d21ed9dbc209e4725a14d32486a3efd4ed341
Kaja Anna <kaja.szokalska@gmail.com>
2021-12-14 13:28:03 +0100

    change in heders allowed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
      'Access-Control-Allow-Headers', 'Origin,Access-Control-Allow-Origin, X-Requested, Content-Type, Accept');
-     res.header('Access-Control-Allow-Methods','POST');
+     res.header('Access-Control-Allow-Methods','POST', 'GET');
     next();
 })
 
