0ddd7aef28283d78bf5a0b49a1b8455209a2e9f6
Kaja Anna <kaja.szokalska@gmail.com>
2021-12-14 13:38:53 +0100

    change error handling

--- app.js
+++ app.js
@@ -10,19 +10,22 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
      'Access-Control-Allow-Headers', 'Origin,Access-Control-Allow-Origin, X-Requested, Content-Type, Accept');
-     res.header('Access-Control-Allow-Methods','POST', 'GET');
+     res.header('Access-Control-Allow-Methods','POST, GET');
     next();
 })
 
 app.use('/api', routes);
 
-app.use((error, req, res, next) => {
-    if (res.headerSent) {
-        return next(error);
-    }
-    res.status(error.code || 500);
-    res.json({ message: error.message || 'An unknown error occurred!'})
-    
-})
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route!', 404);
+    throw error;
+});
 
-app.listen(5000);
+app.use((error, req, res, next) => {
+    if (res.headerSend) {
+        return next();
+    }
+    res.status(error.code || 500)
+    .json({message: error.message || 'Something went wrong!'});
+    
+});
